34d8aec355c77f61f2ababf67ef92983c6c185a4
patrick shen <tianhuai.shen@gmail.com>
2023-03-05 13:50:32 +0800

    updated home.blade.php

--- resources/views/livewire/static-pages/home.blade.php
+++ resources/views/livewire/static-pages/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.default')
 @section('title','my homepage')
 @section('content')
-  <div class="bg-blue-200 p-2  m-3 ">
+  <div class="bg-slate-200 p-2  m-3 ">
     <h1 class="text-3xl mb-4 first-line:font-bold underline"> Hello Laravel</h1>
 
     <p class="mb-5">
-        你现在所看到的是<a href="#">Laravel入门课程</a>的示例项目。
+        你现在所看到的是<a href="#" class="text-blue-600">Laravel入门课程</a>的示例项目。
 
     </p>
 
@@ -15,7 +15,7 @@
     </p>
 
     <p>
-        <button class="border-1 hover:rounded-lg w-24 bg-indigo-400 rounded-full" onclick="window.open('https://www.baidu.com')" >现在注册</button>
+        <button class="border-1 hover:rounded-lg w-24 bg-green-400 rounded-full" onclick="window.open('https://www.baidu.com')" >现在注册</button>
     </p>
   </div>
 
